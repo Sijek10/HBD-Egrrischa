ca0c91bb0d21779732a43b926794947501910baf
Sijek10 <zeky.rahmat11555@gmail.com>
2026-06-27 11:44:38 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,14 @@
 // HBD Egrrischa
-
 document.addEventListener("DOMContentLoaded", function () {
-
     const startBtn = document.getElementById("startBtn");
 
     startBtn.addEventListener("click", function () {
-
+        fireworks();
         startBtn.innerHTML = "Membuka Kejutan... ❤️";
-
         startBtn.disabled = true;
 
         setTimeout(function () {
-
             window.location.href = "pages/letter.html";
-
         }, 1500);
-
     });
-
 });
